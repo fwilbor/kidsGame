736da5abe8095c4a42bfa1e9bf4cd109e52de95b
fwilbor <franklin.cfg@gmail.com>
2019-10-22 18:05:48 -0400

    removes bootstrap/dist/css/bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import AppBar from '@material-ui/core/AppBar';
 
 ReactDOM.render(<App />, document.getElementById('root'));
